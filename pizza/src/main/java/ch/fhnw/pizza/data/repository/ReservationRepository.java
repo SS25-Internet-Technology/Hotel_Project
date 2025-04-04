a261ad5d3fbe657337bdbc2b57e43dfa4592f017
deliia-l <delia.sommet@students.fhnw.ch>
2025-04-04 15:57:03 +0200

    Create ReservationRepository.java

---
+++ pizza/src/main/java/ch/fhnw/pizza/data/repository/ReservationRepository.java
@@ -0,0 +1,17 @@
+package ch.fhnw.pizza.data.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.fhnw.pizza.data.domain.Reservation;
+
+@Repository
+public interface ReservationRepository.java extends JpaRepository<Reservation, Long> {
+    /*
+    List<Reservation> findByStartDateBetween(LocalDate start, LocalDate end);
+    List<Reservation> findByRooms_RoomName(String roomName);
+    */
+}
